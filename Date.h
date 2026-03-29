4a6b964aa0faca93ca9230a16fd91041c0aabf1b
Taiwo-K <TaiwoOlajide@my.unt.edu>
2026-03-29 11:44:29 -0500

    Create Date.h

---
+++ Date.h
@@ -0,0 +1,11 @@
+#ifndef DATE_H
+#define DATE_H
+class Date {
+private:
+    int day, month, year;
+public:
+    Date(int d=1, int m=1, int y=2000);
+    int getAge();
+    void display();
+};
+#endif
