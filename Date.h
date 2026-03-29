61a8e66a2cd1b67f38f444b064f04359ea29238a
hameedid <hameedaholoko56@gmail.com>
2026-03-29 18:35:36 -0500

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,13 +1,14 @@
 #ifndef DATE_H
 #define DATE_H
 
-//Date class
+// Date class
 class Date {
-private:
-    int day, month, year;
 public:
-    Date(int d=1, int m=1, int y=2000);
+    int day, month, year;
+
+    Date(int d = 1, int m = 1, int y = 2000);
     int getAge();
     void display() const;
 };
+
 #endif
