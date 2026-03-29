1301ca7ee637278dda2c3f6710c1cfbb30bf421a
Taiwo-K <TaiwoOlajide@my.unt.edu>
2026-03-29 12:08:58 -0500

    Create Driver.h

---
+++ Driver.h
@@ -0,0 +1,30 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+#include <string>
+#include "Date.h"
+#include "Address.h"
+#include "Ticket.h"
+using namespace std;
+
+class Driver{
+protected:
+    int driverID;
+    string name;
+    string licenseDate;
+    int experience;
+    string workType;
+    string medicalStatus;
+    Date dob;
+    Address address;
+public:
+    Driver(int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
+    virtual void display();
+    int getID();
+    int calculateAge();
+};
+class StudentDriver: public Driver {
+public:
+    StudentDriver( int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
+    void display();
+};
