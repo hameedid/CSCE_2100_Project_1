65fa371052fcbf23817a951a182626361f660292
Taiwo-K <TaiwoOlajide@my.unt.edu>
2026-03-29 12:28:13 -0500

    Update Address.cpp

--- Address.cpp
+++ Address.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 #include "Address.h"
 
+// Address class
 Address::Address(string cty, string str, string ctry, string zip) {
         city = cty;
         street = str;
         country = ctry;
         zipCode = zip;
 }
+
+// displaying the address information
 void Address::display(){
   cout<< street << ", " << city << ", " << country << " " << zipCode << endl;
 }
