810eea8084188c243835fd4eb07062d93945f930
Taiwo-K <TaiwoOlajide@my.unt.edu>
2026-03-29 12:29:47 -0500

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,14 +1,21 @@
-#include <iostream>
+#inlude <iostream>
+#include <string>
 using namespace std;
+#include "Date.h"
 
+//Date class
 Date::Date(int d, int m, int y){
   day=d; month=m, year=y;
 }
 
+// calculating the age of the driver
 int Date::getAge() {
         int currentYear = 2026;
         return currentYear - year;
 }
-void display() const {
+
+// displaying the date information
+void Date::display() const {
         cout << day << "/" << month << "/" << year << endl;
 }
+
