3045aee1e2aae4194c1021714545e0b243e52171
Taiwo-K <TaiwoOlajide@my.unt.edu>
2026-03-29 12:22:35 -0500

    Update Driver.h

--- Driver.h
+++ Driver.h
@@ -28,3 +28,15 @@ public:
     StudentDriver( int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
     void display();
 };
+class MiddleAgeDriver: public Driver {
+public:    MiddleAgeDriver(int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
+    void display();
+};
+class SeniorDriver: public Driver {
+public:    SeniorDriver(int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
+    void display();
+};
+
+Driver* createDriver(int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
+#endif
+
