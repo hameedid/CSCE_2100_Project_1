fd43dd54cb9d5d10fcd0ca41a382dfd2dc7382f6
Taiwo-K <TaiwoOlajide@my.unt.edu>
2026-03-29 12:26:03 -0500

    Update Driver.h

--- Driver.h
+++ Driver.h
@@ -7,6 +7,7 @@
 #include "Ticket.h"
 using namespace std;
 
+// Base class for Driver
 class Driver{
 protected:
     int driverID;
@@ -23,20 +24,28 @@ public:
     int getID();
     int calculateAge();
 };
+
+//class for Student Driver
 class StudentDriver: public Driver {
 public:
     StudentDriver( int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
     void display();
 };
+
+//class for middle age driver
 class MiddleAgeDriver: public Driver {
 public:    MiddleAgeDriver(int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
     void display();
 };
+
+//class for senior driver
 class SeniorDriver: public Driver {
 public:    SeniorDriver(int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
     void display();
 };
 
+//function to create driver based on age
 Driver* createDriver(int id, string n, Date l_date, int exp, string w_type, string m_status, Date b_date, Address a);
+
 #endif
 
