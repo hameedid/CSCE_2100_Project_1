bcbbf2a541019ce8900afc5171227fc895233177
Taiwo-K <TaiwoOlajide@my.unt.edu>
2026-03-29 11:57:27 -0500

    Create Ticket.h

---
+++ Ticket.h
@@ -0,0 +1,19 @@
+#ifndef TICKET_H
+#define TICKET_H
+
+#include <string>
+#include "Date.h"
+using namespace std;
+
+class Ticket {
+private:
+    int ticketID;
+    string location;
+    Date issueDate;
+    
+public:
+    Ticket(int id=0, string loc="", Date d=Date());
+    void display();
+};
+
+#endif
